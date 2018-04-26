8d933504f1b15e98220a8d6a1e4f7fcb7640bac4
MalachiBlackburn <32073221+MalachiBlackburn@users.noreply.github.com>
2018-04-26 14:34:45 -0400

    Add files via upload

---
+++ Blackburn_Classwork_4-26.py
@@ -0,0 +1,37 @@
+##This program uses the pet class
+import pet
+
+def main():
+    ##local variables
+    pet_name = ""
+    pet_type = ""
+    pet_age = 0
+
+
+    pet_name = input("Enter the pet's name: ")
+    pet_type = input("Enter the type of pet: ")
+    pet_age = int(input("Enter the pet's age: "))
+
+    ##Create an instance of the pet class
+
+    mypet = pet.Pet(pet_name, pet_type, pet_age)
+
+    ## Display pet Info
+
+    print("Here is your pet's name: ", mypet.get_name())
+    print("Here is your pet's type: ", mypet.get_animal_type())
+    print("Here is your pet's age: ", mypet.get_age())
+    
+
+
+
+
+
+
+
+
+
+
+
+
+main()
